696eecaa9c8ae417996de5d8039feeca85782da9
Christian Trolle Mikkelsen <christian.mikkelsen@lego.com>
2025-12-21 21:29:49 +0100

    Update scripts

--- bootstrap/bootstrap-cluster-cpl.sh
+++ bootstrap/bootstrap-cluster-cpl.sh
@@ -11,27 +11,36 @@ configPath=state/$clusterName
 
 mkdir -p $configPath
 
+echo "Bootstrapping control plane node at $cplIp"
+echo " cluster: $clusterName"
+echo " state: $configPath"
+
 talosctl gen config $clusterName https://$cplIp:6443 --config-patch @cni-patch.yml --output-dir $configPath --force
 talosctl apply-config --insecure --nodes $cplIp --file $configPath/controlplane.yaml
 
-export TALOSCONFIG="$configPath/talosconfig"
+export TALOSCONFIG=$(readlink -f "$configPath/talosconfig")
 talosctl --talosconfig $TALOSCONFIG config endpoint $cplIp
 talosctl --talosconfig $TALOSCONFIG config node $cplIp
 
-sleep 180
+echo " - Talosconfig exported: $TALOSCONFIG"
+echo " - Waiting for Talos control plane to be ready for bootstrap..."
+echo " - Press any key to continue..."
+read -n 1 -s
 
 talosctl --talosconfig $TALOSCONFIG bootstrap
 
 talosctl --talosconfig $TALOSCONFIG kubeconfig .
 export KUBECONFIG=$(pwd)/kubeconfig  
 
+echo " - Talosconfig after bootstrap and kubeconfig export: $TALOSCONFIG"
+
 while true; do
   current_node_count=$(kubectl get nodes --no-headers | wc -l)
   if [[ $current_node_count -ge 0 ]]; then
-    echo "Cpl node appeared."
+    echo " - Cpl node appeared."
     break
   else
-    echo "Waiting for cpl node..."
+    echo " - Waiting for cpl node..."
     sleep 5
   fi
 done
--- bootstrap/bootstrap-cluster-worker.sh
+++ bootstrap/bootstrap-cluster-worker.sh
@@ -7,19 +7,30 @@ usage() {
 
 clusterName=$1
 workerIp=$2
-configPath=$clusterName
+configPath=state/$clusterName
 
 mkdir -p $configPath
 
+echo "Bootstrapping worker node at $workerIp"
+echo " cluster: $clusterName"
+echo " state: $configPath"
+echo " talosconfig before export: $TALOSCONFIG"
+export TALOSCONFIG=$(readlink -f "$configPath/talosconfig")
+echo " talosconfig after export: $TALOSCONFIG"
+
 talosctl apply-config --insecure --nodes $workerIp --file $configPath/worker.yaml
+
+echo " - Waiting for Talos worker to be ready..."
+echo " - Press any key to continue..."
+read -n 1 -s
 
 while true; do
   current_node_count=$(kubectl get nodes --no-headers | wc -l)
   if [[ $current_node_count -ge 1 ]]; then
-    echo "Worker node appeared."
+    echo " - Worker node appeared."
     break
   else
-    echo "Waiting for worker node..."
+    echo " - Waiting for worker node..."
     sleep 5
   fi
 done
