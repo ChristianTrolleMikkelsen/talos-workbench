46440e983307a3cdb1dcdb4267a671799455c293
Christian Trolle Mikkelsen <christian.mikkelsen@lego.com>
2025-12-21 18:17:47 +0100

    Git work

--- bootstrap/bootstrap-all.sh
+++ bootstrap/bootstrap-all.sh
@@ -5,7 +5,8 @@ clusterName=talos-$1
 (cd ../cluster-state && ./add-cluster-env.sh $clusterName)
 cd ../bootstrap
 
-#./bootstrap-cluster-cpl.sh $clusterName "$2"
-#./bootstrap-cluster-worker.sh $clusterName "$3"
-#./bootstrap-assign-worker-node-roles.sh $clusterName
-#./bootstrap-install-fluxcd.sh $clusterName
+./bootstrap-cluster-cpl.sh $clusterName "$2"
+./bootstrap-cluster-worker.sh $clusterName "$3"
+./bootstrap-cluster-worker.sh $clusterName "$4"
+./bootstrap-assign-worker-node-roles.sh $clusterName
+./bootstrap-install-fluxcd.sh $clusterName
--- cluster-state/add-cluster-env.sh
+++ cluster-state/add-cluster-env.sh
@@ -11,3 +11,7 @@ mkdir -p $clusterName
 
 ./update-all-infra.sh
 ./update-all-apps.sh
+
+git add $clusterName
+git commit -m "Added cluster environment $clusterName"
+git push origin main
