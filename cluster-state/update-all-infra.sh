e89b26912d7889d78fcaafe784cd070d1d79fa4d
Christian Trolle Mikkelsen <christian.mikkelsen@lego.com>
2025-12-22 18:12:33 +0100

    Use stupid helm correctly

--- cluster-state/update-all-infra.sh
+++ cluster-state/update-all-infra.sh
@@ -3,7 +3,7 @@ infraDir="infra-templates"
 mkdir -p $infraDir
 
 helm repo add cilium https://helm.cilium.io/ > /dev/null
-helm repo add trivy https://helm.cilium.io/ > /dev/null
+helm repo add aqua https://aquasecurity.github.io/helm-charts/ > /dev/null
 helm repo update > /dev/null
 
 echo "Updating infra components"
@@ -20,19 +20,19 @@ helm template \
     --set securityContext.capabilities.ciliumAgent="{CHOWN,KILL,NET_ADMIN,NET_RAW,IPC_LOCK,SYS_ADMIN,SYS_RESOURCE,DAC_OVERRIDE,FOWNER,SETGID,SETUID}" \
     --set securityContext.capabilities.cleanCiliumState="{NET_ADMIN,SYS_ADMIN,SYS_RESOURCE}" \
     --set cgroup.autoMount.enabled=false \
-    --set cgroup.hostRoot=/sys/fs/cgroup > $infraDir/cilium.yaml
+    --set cgroup.hostRoot=/sys/fs/cgroup \
+    --include-crds \
+    --output-dir $infraDir
 
 echo " - Updating Spegel"
-spegelns=$(kubectl create namespace spegel --dry-run=client -o yaml | sed '/name: spegel/a\
+helm template spegel oci://ghcr.io/spegel-org/helm-charts/spegel -n spegel --create-namespace --include-crds -f spegel-values.yaml --output-dir $infraDir
+kubectl create namespace spegel --dry-run=client -o yaml | sed '/name: spegel/a\
   labels:\
-    pod-security.kubernetes.io/enforce: privileged')
-helm template spegel oci://ghcr.io/spegel-org/helm-charts/spegel -n spegel -f spegel-values.yaml > $infraDir/spegel.yaml
-printf "%s\n%s\n" "---" "$spegelns" >> $infraDir/spegel.yaml
+    pod-security.kubernetes.io/enforce: privileged' > $infraDir/spegel/templates/namespace.yaml
 
 echo " - Updating Trivy"
-trivyns=$(kubectl create namespace trivy-system --dry-run=client -o yaml)
-helm template trivy-operator oci://ghcr.io/aquasecurity/helm-charts/trivy-operator -n trivy-system -f trivy-values.yaml > $infraDir/trivy.yaml
-printf "%s\n%s\n" "---" "$trivyns" >> $infraDir/trivy.yaml
+helm template trivy-operator aqua/trivy-operator -n trivy-system --create-namespace --include-crds -f trivy-values.yaml --output-dir $infraDir
+kubectl create namespace trivy-system --dry-run=client -o yaml > $infraDir/trivy-operator/templates/namespace.yaml
 
 for folder in "."/*; do
   if [ -d "$folder" ]; then
