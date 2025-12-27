9919ded9432268984c234387ced07eb9b9191620
Christian Trolle Mikkelsen <christian.mikkelsen@lego.com>
2025-12-27 11:44:30 +0100

    Fix so i can port forward to test endpoints locally

--- cluster-state/update-all-infra.sh
+++ cluster-state/update-all-infra.sh
@@ -36,6 +36,7 @@ kubectl create namespace spegel-system --dry-run=client -o yaml | sed '/name: sp
 echo " - Updating Trivy"
 helm template trivy-operator aqua/trivy-operator -n trivy-system --create-namespace --include-crds -f trivy-values.yaml --output-dir $infraDir
 kubectl create namespace trivy-system --dry-run=client -o yaml > $infraDir/trivy-operator/templates/namespace.yaml
+sed -i '' '/clusterIP: None/d' "$infraDir/trivy-operator/templates/monitor/service.yaml"
 
 echo " - Updating Local Path Storage Provider"
 echo "   - Setting Talos data disk path /var/mnt/data"
