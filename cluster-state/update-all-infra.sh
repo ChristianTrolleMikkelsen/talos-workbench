ec27a7b0330748c604819b21e5f258e01bd9b2bb
Christian Trolle Mikkelsen <christian.mikkelsen@lego.com>
2025-12-27 16:30:00 +0100

    More cilium metrics

--- cluster-state/update-all-infra.sh
+++ cluster-state/update-all-infra.sh
@@ -24,6 +24,11 @@ helm template \
     --set securityContext.capabilities.cleanCiliumState="{NET_ADMIN,SYS_ADMIN,SYS_RESOURCE}" \
     --set cgroup.autoMount.enabled=false \
     --set cgroup.hostRoot=/sys/fs/cgroup \
+    --set prometheus.enabled=true \
+    --set operator.prometheus.enabled=true \
+    --set hubble.enabled=true \
+    --set hubble.metrics.enableOpenMetrics=true \
+    --set hubble.metrics.enabled="{dns,drop,tcp,flow,port-distribution,icmp,httpV2:exemplars=true;labelsContext=source_ip\,source_namespace\,source_workload\,destination_ip\,destination_namespace\,destination_workload\,traffic_direction}" \
     --set hubble.ui.enabled=true \
     --set hubble.relay.enabled=true \
     --include-crds \
